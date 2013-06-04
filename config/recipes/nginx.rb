e96209be1dfb63d19d793f421d8f2e23c00d043a
Octavio <octanks@hotmail.com>
2013-06-03 21:34:07 -0300

    deploy files

--- config/recipes/nginx.rb
+++ config/recipes/nginx.rb
@@ -10,8 +10,8 @@ namespace :nginx do
   desc "Setup nginx configuration for this application"
   task :setup, roles: :web do
     template "nginx_unicorn.erb", "/tmp/nginx_conf"
-    run "#{sudo} mv /tmp/nginx_conf /etc/nginx/sites-enabled/#{application}"
-    run "#{sudo} rm -f /etc/nginx/sites-enabled/default"
+    run "mv /tmp/nginx_conf /etc/nginx/sites-enabled/#{application}"
+    run "rm -f /etc/nginx/sites-enabled/default"
     restart
   end
   after "deploy:setup", "nginx:setup"
@@ -19,7 +19,7 @@ namespace :nginx do
   %w[start stop restart].each do |command|
     desc "#{command} nginx"
     task command, roles: :web do
-      run "#{sudo} service nginx #{command}"
+      run "service nginx #{command}"
     end
   end
 end
