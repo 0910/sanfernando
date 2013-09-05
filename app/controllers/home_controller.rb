846dd060a987f7ad956e6d576ac06087851243d4
agiesenow <andres.giesenow@gmail.com>
2013-09-04 21:07:10 -0300

    last design 4-9

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController 
   def index
-		@calendars = Calendar.last(4)
+		@calendars = Calendar.last(2)
 		@news = News.order('created_at asc').last(6)
   end
 end
