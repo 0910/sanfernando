b3211c651a81562d7a7a7777a70948ca738a4e4e
Nuevediez <e@nuevediez.com.ar>
2014-12-12 15:58:45 -0300

    home news order change

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController 
   def index
 		@calendars = Calendar.last(2)
-		@news = News.order('created_at desc').last(6)
+		@news = News.order('created_at desc').first(6)
   end
 end
