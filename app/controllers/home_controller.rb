bee5a7dae7bf7cc3e8e52de74cd3859db1fe0222
Nuevediez <e@nuevediez.com.ar>
2014-12-12 11:55:05 -0300

    Actualizacion home 12 de Diciembre

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController 
   def index
 		@calendars = Calendar.last(2)
-		@news = News.order('created_at asc').last(6)
+		@news = News.order('created_at desc').last(6)
   end
 end
